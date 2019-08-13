1613f561b840b1f781b2d86b805332aa3d74bd20
tranminhlong0000 <tranminhlong0000@gmail.com>
2019-08-13 17:25:51 +0700

    HomeScreen: FeedItem Update

--- components/FlatList/FeedItem.js
+++ components/FlatList/FeedItem.js
@@ -1,14 +1,35 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, Button, Linking } from "react-native";
+import { timeSince } from "../../common/utils";
 
 onPressReadmore = url => {
   Linking.openURL(url).catch(err => console.error(err));
 };
-const FeedItem = ({ item: { urlToImage, title, url } }) => {
+const imageNotFound =
+  "https://t4.ftcdn.net/jpg/01/39/16/63/240_F_139166369_NdTDXc0lM57N66868lC66PpsaMkFSwaf.jpg";
+const FeedItem = ({
+  item: { urlToImage, title, content, url, source, publishedAt }
+}) => {
   return (
     <View style={styles.cardContainer}>
-      <Image source={{ uri: urlToImage }} style={styles.image} />
-      <Text>{title}</Text>
+      <View style={styles.title}>
+        <Text style={styles.textBold}>{title}</Text>
+      </View>
+
+      <Image
+        source={{ uri: urlToImage || imageNotFound }}
+        style={styles.image}
+      />
+      <Text style={styles.textBold}>
+        Source: <Text style={styles.textNormal}>{source.name}</Text>
+      </Text>
+      <Text style={styles.textNormal}>
+        {content || "Does not have content"}
+      </Text>
+      <Text style={styles.textBold}>
+        Published:{" "}
+        <Text style={styles.textNormal}>{timeSince(publishedAt) + " ago"}</Text>
+      </Text>
       <Button
         style={styles.button}
         title="Read more"
@@ -24,15 +45,29 @@ const styles = StyleSheet.create({
   cardContainer: {
     flexDirection: "column",
     paddingVertical: 10,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
+    borderColor: "#808080",
+    borderWidth: 1,
+    marginBottom: 5,
+    justifyContent: "center"
+  },
+  title: {
+    alignItems: "center",
+    justifyContent: "center"
   },
   image: {
-    width: 400,
+    width: 360,
     height: 200
   },
   button: {
     backgroundColor: "blue",
     paddingVertical: 15,
     borderRadius: 10
+  },
+  textBold: {
+    fontWeight: "800"
+  },
+  textNormal: {
+    fontWeight: "100"
   }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,9 @@ import {
   View,
   StyleSheet,
   ActivityIndicator,
-  FlatList
+  FlatList,
+  StatusBar,
+  ToastAndroid
 } from "react-native";
 import FeedItem from "../components/FlatList/FeedItem";
 import { week5Api } from "../common/axios";
@@ -14,24 +16,60 @@ export default class HomeScreen extends React.Component {
     isLoading: false,
     articles: [],
     totalResults: 0,
-    page: 1
+    page: 1,
+    error: "",
+    isLoadMore: false,
+    isRefreshing: false
   };
-  async componentDidMount() {
+  componentDidMount() {
+    const { page } = this.state;
     this.setState({ isLoading: true });
-    const res = await week5Api.get(
-      "/top-headlines?country=us&apiKey=9518dd1b64ee47e19d0f6e05007b7c16&page=1"
-    );
-    this.setState({
-      isLoading: false,
-      articles: res.data.articles,
-      totalResults: res.data.totalResults
-    });
+    this.callApi(page);
   }
+
+  callApi = async page => {
+    try {
+      const { articles } = this.state;
+      const res = await week5Api.get(
+        `/top-headlines?country=us&apiKey=9518dd1b64ee47e19d0f6e05007b7c16&page=${page}`
+      );
+      this.setState({
+        page,
+        isLoading: false,
+        articles: articles.concat(res.data.articles),
+        totalResults: res.data.totalResults,
+        isRefreshing: false
+      });
+    } catch (error) {
+      ToastAndroid.show("Loading articles fail", ToastAndroid.SHORT);
+      this.setState({ error });
+    }
+  };
 
   renderItem = ({ item }) => <FeedItem item={item} />;
 
+  onFlatListEnd = async () => {
+    const { page } = this.state;
+    const newPage = page + 1;
+    this.callApi(newPage);
+  };
+
+  onFlatListRefresh = async () => {
+    const firstPage = 1;
+    await this.setState({ isLoading: true, isRefreshing: true, articles: [] });
+    this.callApi(firstPage);
+  };
+
+  renderFooter = () => (
+    <ActivityIndicator
+      size="large"
+      color="blue"
+      animating={!this.state.refreshing}
+    />
+  );
+
   render() {
-    const { isLoading, articles } = this.state;
+    const { isLoading, articles, isRefreshing, totalResults } = this.state;
     if (isLoading) {
       return (
         <View style={styles.container}>
@@ -41,11 +79,18 @@ export default class HomeScreen extends React.Component {
     }
     return (
       <View style={styles.container}>
+        <Text style={styles.textTotalArticles}>Articles: {totalResults}</Text>
         <FlatList
           style={styles.flatList}
           data={articles}
-          keyExtractor={item => item.content}
+          keyExtractor={(item, index) => item.description + index.toString()}
           renderItem={this.renderItem}
+          onEndReached={this.onFlatListEnd}
+          onEndReachedThreshold={0.6}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.onFlatListRefresh}
+          refreshing={isRefreshing}
+          
         />
       </View>
     );
@@ -62,7 +107,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 15
+    marginTop: StatusBar.currentHeight
+  },
+  textTotalArticles: {
+    fontWeight: "500"
   },
   flatList: {
     marginHorizontal: 15
